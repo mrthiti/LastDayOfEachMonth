6663d4f6b4e86845cde888c71b02d064396d8bd8
Thiti Yamsung <thiti.59h@gmail.com>
2017-07-14 00:22:19 +0700

    Start project

---
+++ LastDayOfEachMonth/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim year = 2017
+        Dim arrayLastDay = getLastDayOfAllMunth(year)
+
+        Console.WriteLine("Last day of each month in year " & year)
+
+        For i As Integer = 0 To arrayLastDay.Length - 1
+            Console.WriteLine(arrayLastDay(i))
+        Next
+
+        Console.ReadKey()
+    End Sub
+
+    Function getLastDayOfAllMunth(ByVal year As Long) As String()
+        Dim thisYear = New DateTime(year, 1, 1)
+        Dim lastDayOfAllMonth(11) As String
+
+        For i As Integer = 0 To lastDayOfAllMonth.Length - 1
+            lastDayOfAllMonth(i) = thisYear.AddMonths(i + 1).AddDays(-1).ToString("dd/MM/yyyy")
+        Next
+
+        Return lastDayOfAllMonth
+    End Function
+
+End Module
